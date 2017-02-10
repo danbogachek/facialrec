304d3a26b6fca7be646bcbf42366ba0517a9ca2c
danbogachek <bogachek.daniel@gmail.com>
2017-02-10 20:48:37 +0000

    works with still images

--- extract.py
+++ extract.py
@@ -1,19 +1,45 @@
 import numpy as np
 import cv2
 import sys
+import subprocess as sub
 
+
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+from time import sleep
+
+file = "processed.jpg"
+
+camera = PiCamera()
+camera.framerate = 32
+
+
+def process(img):
+	pass
+	
+
+
+
+rawCapture = PiRGBArray(camera)
+sleep(.1)
+camera.capture(rawCapture, format="bgr")
+
+img = rawCapture.array
 
 facer = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eyer = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread(sys.argv[1])
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 faces = facer.detectMultiScale(gray, 1.3, 5)
 
 for (x,y,w,h) in faces:
 	cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
 
+if len(faces) < 1:
+	print "no faces detected"
 
-cv2.imwrite("processed.jpg", img)
-print "done"
+else:
+
+	cv2.imwrite(file, img)
+	substring = "sudo fbi -d /dev/fb0 -a -T 2 {}".format(file)
+	sub.call(substring, shell=True)
+	print "done"
