5fbff0dfe39d586c5b9310ce28d9022145b5f7c8
danbogachek <bogachek.daniel@gmail.com>
2017-02-13 05:33:40 +0000

    continuous extraction works

--- extract.py
+++ extract.py
@@ -11,35 +11,53 @@ from time import sleep
 file = "processed.jpg"
 
 camera = PiCamera()
-camera.framerate = 32
+#camera.framerate = 32
+
+facer = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 def process(img):
-	pass
+	
+	global facer
+
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = facer.detectMultiScale(gray, 1.3, 5)
+	
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+	
+	if len(faces) < 1:
+		print "no faces detected"
+
+	else:
+		cv2.imwrite(file, img)
+		substring = "sudo fbi -d /dev/fb0 -a -T 2 {}".format(file)
+		sub.call(substring, shell=True)
+		print "done"
 	
 
 
 
 rawCapture = PiRGBArray(camera)
 sleep(.1)
-camera.capture(rawCapture, format="bgr")
+#camera.capture(rawCapture, format="bgr")
 
-img = rawCapture.array
 
-facer = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+while True:
+	camera.capture(rawCapture, format = "bgr")
+	img = rawCapture.array
+	process(img)
+	rawCapture.truncate(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = facer.detectMultiScale(gray, 1.3, 5)
 
-for (x,y,w,h) in faces:
-	cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+'''
+for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port=True):
+	
+	img = frame.array
+	process(img)
+	#rawCapture.truncate(0)
+	sleep(2)
 
-if len(faces) < 1:
-	print "no faces detected"
 
-else:
-
-	cv2.imwrite(file, img)
-	substring = "sudo fbi -d /dev/fb0 -a -T 2 {}".format(file)
-	sub.call(substring, shell=True)
-	print "done"
+#process(img)
+'''
